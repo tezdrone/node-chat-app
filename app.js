50f91d622ee6be07b2f2772db47758cdedc61162
Ben <mineeraft@gmail.com>
2021-03-15 22:56:11 +1300

    Add socket support

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,10 +19,15 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
 	console.log(req.body);
+	io.emit('message', req.body);
 	messages.push(req.body);
 	res.sendStatus(200);
 });
 
-const server = app.listen('3000', (err) => {
+io.on('connection', (socket) => {
+	console.log('User Connected');
+});
+
+const server = http.listen('3000', (err) => {
 	console.log(`Server runnning on port ${server.address().port}`);
 });
